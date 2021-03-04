020e63370055219414f80d521ca0d98793ddbdc1
Kasidid Waiyakul <kasidid.wai@lightnet.io>
2021-03-04 18:06:24 +0700

    add functions

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ let ins = await Citizen.deployed()
 
 ins.assignOfficer()
 
-ins.register("0x1F2333ad7BA1Ff62c64c97965C79b97B90a8be5E", "Gun", "LN")
+ins.register("0xe729Ae245c2B16dB8Ac99f3c31B4Bf8e976e652b", "Gun", "LN","123",1,1,"0xe729Ae245c2B16dB8Ac99f3c31B4Bf8e976e652b","0xe729Ae245c2B16dB8Ac99f3c31B4Bf8e976e652b","123456")
 
-ins.get("0x1F2333ad7BA1Ff62c64c97965C79b97B90a8be5E")
+ins.get("0xe729Ae245c2B16dB8Ac99f3c31B4Bf8e976e652b")
--- contracts/Citizen.sol
+++ contracts/Citizen.sol
@@ -1,44 +1,91 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.25 <0.9.0;
+pragma experimental ABIEncoderV2;
+
 
 contract Citizen {
-	event NewRegister(address person, string firstname, string lastName);
+	event NewRegister(address addressNumber, 
+	string firstName, 
+	string lastName,
+	uint dob, 
+	Gender gender, 
+	Status status, 
+	address father, 
+	address mother, 
+	uint idNumber);
 
 	address public officer;
 
-	enum Status { live, dead }
+	enum Status { live, dead, meff }
 	enum Gender { Male, Female }
 
 	struct Person {
-		// address ownerSecret; hash key
-		string firstName;
+		address addressNumber; 
+		string firstName; 
 		string lastName;
-		// uint dob;
-		// Gender gender;
-		// Status status;
-		// address father;
-		// address mother;
-		// address addressNumber;
-		// uint idNumber;
+		uint dob; 
+		Gender gender; 
+		Status status; 
+		address father; 
+		address mother; 
+		uint idNumber;
 	}
 
 	mapping(address => Person) public peoples;
 
-	
-
-	function register(address newId, string memory _firstName, string memory _lastName) public {
+	function register(address _address, 
+		string memory _firstName, 
+		string memory _lastName,
+		uint _dob, 
+		Gender _gender, 
+		Status _status, 
+		address _father, 
+		address _mother, 
+		uint _idNumber
+	) public {
 		require(msg.sender == officer, "Unauthorized");
-
-		peoples[newId] = Person(_firstName, _lastName);
-
-		emit NewRegister(newId, _firstName, _lastName);
+		peoples[_address] = Person(_address, _firstName, _lastName, _dob, _gender, _status, _father, _mother, _idNumber);
+		emit NewRegister(_address, _firstName, _lastName, _dob, _gender, _status, _father, _mother, _idNumber);
 	}
 
 	function assignOfficer() public {
 		officer = msg.sender;
 	}
-	
-	function get(address id) public view returns (string memory) {
-		return peoples[id].firstName;
+
+	function get(address _address) public view returns (Person memory){
+		return peoples[_address];
 	}
+
+	function getStatus(address _address) public view returns (Status) {
+		return peoples[_address].status;
+	}
+
+	function getParent(address _address) public view returns (Person memory, Person memory) {
+	 	Person memory father = get(peoples[_address].father);
+		Person memory mother = get(peoples[_address].mother);
+		return (father, mother);
+	}
+
+	function update(address _address, 
+		string memory _firstName, 
+		string memory _lastName,
+		Status _status
+	) public returns (Person memory) {
+		require(msg.sender == officer, "Unauthorized");
+		Person storage person = peoples[_address];
+		if (!isEmpty(_firstName)) { person.firstName = _firstName; }
+		if (!isEmpty(_lastName)) { person.lastName = _lastName; }
+		if (!isEmptyStatus(_status)) { person.status = _status; }
+		
+		return peoples[_address];
+	}
+
+	function isEmpty(string memory s1) public pure returns(bool){
+		return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(""));
+	}
+
+	function isEmptyStatus(Status s1) public pure returns(bool){
+		return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(""));
+	}
+
 }
